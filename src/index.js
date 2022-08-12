90e6bc901edba5c974641d60e5d8e4099ef7ca57
Misha Gorban <mixei8189@gmail.com>
2022-08-12 08:33:21 +0300

    add нос

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/app';
 import Carousel from './components/app/carousel';
+import Nos from './components/app/Nos';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<StrictMode>
+	<>
+		<Nos/>
 		<Carousel/>
 		<App/>
-	</StrictMode>
+	</>
 );
 
